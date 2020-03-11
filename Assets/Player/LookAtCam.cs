e940da5931bc9dca44e584e9ff65d7a20b934f8b
Wendel <wendelblankjeske@gmail.com>
2020-03-11 17:38:55 -0300

    v0.3.6

--- Assets/Player/LookAtCam.cs
+++ Assets/Player/LookAtCam.cs
@@ -10,12 +10,17 @@ public class LookAtCam : MonoBehaviour
     {
         cam = Camera.main;
     }
-
-    // Update is called once per frame
+	
     private void Update()
     {
         Vector3 toCam = cam.transform.position - transform.position;
         toCam.y = 0;
-        transform.rotation = Quaternion.LookRotation(-toCam, Vector3.up);
+        //transform.rotation = Quaternion.LookRotation(-toCam, Vector3.up);
     }
+
+	private void LateUpdate()
+	{
+		transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward,
+			cam.transform.rotation * Vector3.up);
+	}
 }
